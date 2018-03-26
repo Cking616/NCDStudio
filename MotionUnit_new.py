74eb854d729991e99484925b95ab00f62676f41d
cking616 <cking616@mail.ustc.edu.cn>
2018-03-26 12:56:37 +0800

    new motionunit

--- MotionUnit_new.py
+++ MotionUnit_new.py
@@ -12,6 +12,7 @@
 
 import asyncore
 import socket
+import threading
 
 
 class NanotecMotor(asyncore.dispatcher):
@@ -24,18 +25,25 @@ class NanotecMotor(asyncore.dispatcher):
         self.error = ""
         self.canSend = False
         self.lastSend = None
+        self.lock = threading.RLock()
 
     def handle_connect(self):
+        self.lock.acquire()
         self.canSend = True
+        self.lock.release()
 
     def handle_close(self):
+        self.lock.acquire()
         self.canSend = False
+        self.lock.release()
         self.close()
 
     def handle_read(self):
         rec = self.recv(8192)
         if rec == self.lastSend:
+            self.lock.acquire()
             self.canSend = True
+            self.lock.release()
         else:
             self.error = rec
 
@@ -53,27 +61,36 @@ class NanotecMotor(asyncore.dispatcher):
         if not self.canSend:
             return
         sent = self.send(self.sendBuffer.encode('utf-8'))
+        self.lock.acquire()
         self.sendBuffer = self.sendBuffer[sent:]
+        self.lock.release()
         if not self.sendBuffer:
+            self.lock.acquire()
             self.sendBuffer = self.cmdBuffer.pop()
             self.sendBuffer = '#' + self.sendBuffer
             self.canSend = False
+            self.lock.release()
 
     def is_error(self):
         return self.error != ""
 
     def exec_cmd(self, cmd):
+        self.lock.acquire()
         self.cmdBuffer = cmd
         self.sendBuffer = self.cmdBuffer.pop()
         self.sendBuffer = '#' + self.sendBuffer
-        self.canSend = False
+        self.canSend = True
+        self.lock.release()
 
     def is_cmd_end(self):
         return not self.writable()
 
 
 if __name__ == '__main__':
+    import time
+
     xMotor = NanotecMotor(('192.168.100.254', 4001))
+    yMotor = NanotecMotor(('192.168.100.254', 4001))
 
     cmdPro = ['1A\r',
               '1y1\r',
@@ -85,4 +102,23 @@ if __name__ == '__main__':
               '1:D\r',
               '1S\r']
 
-    asyncore.loop()
+    class MyThread(threading.Thread):
+        def run(self):
+            asyncore.loop()
+
+    t = MyThread()
+    t.start()
+
+    xMotor.exec_cmd(cmdPro)
+    while xMotor.is_cmd_end():
+        time.sleep(1)
+
+    print("xMotor Home\n")
+
+    yMotor.exec_cmd(cmdPro)
+    while yMotor.is_cmd_end():
+        time.sleep(1)
+
+    print("yMotor Home\n")
+
+    input("Enter\n")
